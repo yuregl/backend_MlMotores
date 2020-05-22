72b9571d4e4ecfb86e97ff383773219ccf7ee3c8
Yure Galdino <yuregaldino@hotmail.com>
2020-05-22 13:37:06 -0300

    seed e migration de produto

--- src/database/migrations/20200520152336_create_table_product.js
+++ src/database/migrations/20200520152336_create_table_product.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+	return knex.schema.createTable('products', function (table) {
+		table.increments('id');
+		table.string('produto').notNullable();
+		table
+			.enu('status', ['EM_OBSERVACAO', 'FINALIZADO', 'EM_MANUTENCAO'])
+			.notNullable();
+		table.integer('value').notNullable();
+		table.text('description');
+	});
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTable('products');
 };
