5e4e6bde67b0dffb174e0e2cc10a9ab2d479d784
Yure Galdino <yuregaldino@hotmail.com>
2020-05-22 13:34:37 -0300

    rota de Login

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const routes = express.Router();
 const UserController = require('./controllers/UserController');
+const SessionController = require('./controllers/SearchUserController');
 
 routes.post('/register', UserController.create);
 routes.get('/users', UserController.list);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
+
+routes.post('/', SessionController.login);
 
 module.exports = routes;
